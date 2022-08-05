27143fdba2b33d511c0cdb514e344ff71f6bcde6
Elyes Ferjani <officialef@hotmail.com>
2022-08-05 09:03:46 +0100

    adding terraform vars

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "lambda_function_name"{
+    default = "getComments"
+}
+variable "lambda_function_name_sqs"{
+    default = "putSQS"
+}
+variable "lambda_function_name_sqs_poll"{
+    default = "pollSQS"
+}
